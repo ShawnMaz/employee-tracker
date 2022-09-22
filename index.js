80d0cfd353aa2554ba4098eddb4aa428a3d08fa7
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-09-22 11:31:00 -0400

    modified the CRUD functions

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const inquirer = require('inquirer');
+const db = require('./db/connection');
 require('dotenv').config();
-const {getAllData, postData, updateEmployeeRole} = require('./lib/Data');
+const { getAllData, postData, updateEmployeeRole } = require('./lib/Data');
+const cTable = require('console.table');
+
+const program = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'task',
+            message: 'Please select an operation from the list below: ',
+            choices: ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ]).then(answer => {
+        // console.clear();
+        const data = getAllData('departments');
+        console.table(data);
+    }).then(() => {
+        db.end();
+    })
+};
+
+program();
